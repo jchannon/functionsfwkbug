ed26a111954acbb75e92144ece5f327bc0775954
Jonathan Channon <jonathan.channon@gmail.com>
2021-03-08 11:48:58 +0000

    initla commit

---
+++ ConsoleApp4/Function.fs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4
+
+open Google.Cloud.Functions.Framework
+open System.Threading.Tasks
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+
+type Function(logger: ILogger<Function>, config: IConfiguration) =
+
+    do
+        logger.LogDebug("Hi from Debug")
+        logger.LogInformation("Hi from Info")
+
+
+    interface IHttpFunction with
+        member this.HandleAsync(context) = Task.CompletedTask
